074dfd72722ed307757f8d4964186e7915cc7512
Jason Washburn <jason.washburn@gmail.com>
2026-05-29 07:21:09 -0500

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -7,6 +7,22 @@ import (
 	"strings"
 )
 
+var validCommands = map[string]func([]string) int{
+	"exit": func(_ []string) int {
+		os.Exit(0)
+		return 0
+	},
+	"echo": func(args []string) int {
+		fmt.Println(strings.Join(args[1:], " "))
+		return 0
+	},
+}
+
+func isValidCommand(s string) bool {
+	_, exists := validCommands[s]
+	return exists
+}
+
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	for {
@@ -21,11 +37,9 @@ func main() {
 		raw := scanner.Text()
 		args := strings.Split(raw, " ")
 
-		switch args[0] {
-		case "exit":
-			os.Exit(0)
-		case "echo":
-			fmt.Println(strings.Join(args[1:], " "))
+		switch isValidCommand(args[0]) {
+		case true:
+			validCommands[args[0]](args)
 		default:
 			fmt.Printf("%s: command not found\n", args[0])
 		}
