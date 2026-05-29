09445688b15f7c5b1cf383c96c6490f45fe44c77
Jason Washburn <jason.washburn@gmail.com>
2026-05-28 21:03:02 -0500

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -1,9 +1,24 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
 func main() {
+	scanner := bufio.NewScanner(os.Stdin)
 	fmt.Print("$ ")
+	for scanner.Scan() {
+		command := scanner.Text()
+		switch command {
+		default:
+			fmt.Printf("%s: command not found\n", command)
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "Error reading input:", err)
+		os.Exit(1)
+	}
 }
